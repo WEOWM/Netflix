1a6213dd26cbc8a4d49663ab04ff4163cc30db7d
basim <basims0077@gmail.com>
2022-09-13 09:10:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import Banner from './Commponent/Banner/Banner'
 import NavBar from './Commponent/NavBar/NavBar'
 import Rowpost from './Commponent/Rowpost/Rowpost'
-import{HorrorMovies,ComedyMovies, originals, action ,RomanceMovies}from './url'
+import{HorrorMovies,ComedyMovies, originals, action ,RomanceMovies,trending,Documentaries}from './url'
 
 function App() {
   return (
@@ -11,10 +11,12 @@ function App() {
       <NavBar/>
       <Banner/>
       <Rowpost url={originals} title='NetFlix Originals'/>
+      <Rowpost url={trending} title='Trending'isSmall/>
       <Rowpost url={action} title='Action Movies'isSmall/>
       <Rowpost url={HorrorMovies} title='Horror Movies 'isSmall/>
       <Rowpost url={ComedyMovies} title='Comedy Movies 'isSmall/>
       <Rowpost url={RomanceMovies} title='Romance Movies 'isSmall/>
+      <Rowpost url={Documentaries} title='Documentaries'isSmall/>
       
      
       
